70197eb32efe9f6148f96480d1fbd517a783bbb0
walidzaaneun <zaanoun3@gmail.com>
2026-04-01 14:47:55 +0100

    add injection Par instanciation dynamique

--- src/main/java/com/walidzaaneun/Main.java
+++ src/main/java/com/walidzaaneun/Main.java
@@ -1,13 +1,34 @@
 package com.walidzaaneun;
 
+import com.walidzaaneun.Metier.IMetier;
 import com.walidzaaneun.Metier.MetierImpl;
 import com.walidzaaneun.dao.DaoImpl;
+import com.walidzaaneun.dao.IDao;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         // Injection Par instanciation statique
         DaoImpl daoImpl = new DaoImpl();
         MetierImpl metierImpl = new MetierImpl(daoImpl);
         System.out.println("calcul = "+metierImpl.calcul());
+
+
+        // Injection  Par instanciation dynamique
+        Scanner sc = new Scanner(new File("config.txt"));
+        String doaClassName = sc.nextLine();
+        Class cDao = Class.forName(doaClassName);
+        IDao dao= (IDao) cDao.newInstance();
+
+        String metierClassName = sc.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+        System.out.println("calcul = "+metier.calcul());
+
+
     }
 }
