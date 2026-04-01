07fd0f426fbba52c6251b4c2f429dbe2c23ef350
walidzaaneun <zaanoun3@gmail.com>
2026-04-01 15:14:37 +0100

    add injection En utilisant le Framework Spring Version XML

--- src/main/java/com/walidzaaneun/Main.java
+++ src/main/java/com/walidzaaneun/Main.java
@@ -4,6 +4,8 @@ import com.walidzaaneun.Metier.IMetier;
 import com.walidzaaneun.Metier.MetierImpl;
 import com.walidzaaneun.dao.DaoImpl;
 import com.walidzaaneun.dao.IDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,6 +30,12 @@ public class Main {
         Class cMetier = Class.forName(metierClassName);
         IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
         System.out.println("calcul = "+metier.calcul());
+
+
+        // Injection En utilisant le Framework Spring Version XML
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier1 = springContext.getBean(IMetier.class);
+        System.out.println("calcul = "+metier1.calcul());
 
 
     }
--- src/main/java/com/walidzaaneun/Metier/MetierImpl.java
+++ src/main/java/com/walidzaaneun/Metier/MetierImpl.java
@@ -1,12 +1,19 @@
 package com.walidzaaneun.Metier;
 
+import com.walidzaaneun.dao.DaoImpl;
 import com.walidzaaneun.dao.IDao;
 
 public class MetierImpl implements IMetier{
+    public void setiDao(IDao iDao) {
+        this.iDao = iDao;
+    }
+
     private IDao iDao;
 
     public MetierImpl(IDao iDao) {
         this.iDao = iDao;
+    }
+    public MetierImpl() {
     }
 
     @Override
