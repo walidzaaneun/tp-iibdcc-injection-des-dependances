51b5b65365598d35299d20fd301e27d175fc5d79
walidzaaneun <zaanoun3@gmail.com>
2026-04-01 14:32:53 +0100

    add injection Par instanciation statique

--- src/main/java/com/walidzaaneun/Main.java
+++ src/main/java/com/walidzaaneun/Main.java
@@ -1,6 +1,13 @@
 package com.walidzaaneun;
 
+import com.walidzaaneun.Metier.MetierImpl;
+import com.walidzaaneun.dao.DaoImpl;
+
 public class Main {
     public static void main(String[] args) {
+        // Injection Par instanciation statique
+        DaoImpl daoImpl = new DaoImpl();
+        MetierImpl metierImpl = new MetierImpl(daoImpl);
+        System.out.println("calcul = "+metierImpl.calcul());
     }
 }
